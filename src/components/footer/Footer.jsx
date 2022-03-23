eb04e6434f730e62ceb705dfdff5400133367fc4
Avazbek <foziljonovavazbek0807@gmail.com>
2022-03-24 00:24:50 +0500

    start navbar

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="Footer">
+      <div className="logo">Footer</div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="logo">Navbar</div>
+      <ul className="menu">
+        <li>
+          <a href="#!">Home</a>
+        </li>
+        <li>
+          <a href="#!">Home</a>
+        </li>
+        <li>
+          <a href="#!">Home</a>
+        </li>
+        <li>
+          <a href="#!">Home</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
